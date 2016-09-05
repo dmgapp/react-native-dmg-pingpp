fd18fc917302516575d68d2b81e998a7fcd0f750
wuway <wayivan@126.com>
2016-09-05 10:53:31 +0800

    1. 修改index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,3 +1,53 @@
 /**
  * Created by Zix on 16/9/2.
  */
+
+'use strict'
+
+import { NativeModules, NativeEventEmitter } from 'react-native';
+
+let nativeAPI = NativeModules.RNDMGPingPP;
+const myModuleEvt = new NativeEventEmitter( nativeAPI )
+let savedCallback = undefined;
+
+myModuleEvt.addListener( 'Pingpp_Resp', resp => {
+  const callback = savedCallback;
+savedCallback = undefined;
+callback && callback( resp );
+} );
+
+function waitForResponse() {
+  return new Promise( ( resolve, reject ) => {
+
+    // if ( savedCallback ) {
+    //   savedCallback( '用户中途取消' );
+    // }
+
+    savedCallback = r => {
+    savedCallback = undefined;
+
+    const { result, errCode, errMsg } = r;
+
+    if ( result && result === 'success' ) {
+      resolve( result );
+    } else if ( result && result === 'cancel' ) {
+      resolve( result );
+    } else {
+      const err = new Error( errMsg );
+      err.errCode = errCode;
+      err.errMsg = errMsg;
+      reject( err );
+    }
+  };
+} );
+}
+
+export async function pay( charge ) {
+  if ( typeof charge === 'string' ) {
+    nativeAPI.pay( charge );
+  } else {
+    nativeAPI.pay( JSON.stringify( charge ) );
+  }
+
+  return await waitForResponse();
+}
