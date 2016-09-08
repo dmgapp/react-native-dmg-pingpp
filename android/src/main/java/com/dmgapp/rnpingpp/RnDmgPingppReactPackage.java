ac0035dd8fbedfab712153cd5e0d8719e3ff3097
scott <406844719@qq.com>
2016-09-08 13:50:22 +0800

    commit android pingpp pay

---
+++ android/src/main/java/com/dmgapp/rnpingpp/RnDmgPingppReactPackage.java
@@ -0,0 +1,33 @@
+package com.dmgapp.rnpingpp;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by scott on 16/9/8.
+ */
+public class RnDmgPingppReactPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new RnDmgPingppModule(reactContext));
+        return modules;
+    }
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
